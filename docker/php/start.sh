99017889cd50ebf7148d42a0433e137ded7353dd
rea <m.rakhely.viktoria@gmail.com>
2023-07-02 13:56:17 +0200

    Refactored code  and fixed erorrors

--- docker/php/start.sh
+++ docker/php/start.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+composer install
+
+
 until bin/console doctrine:query:sql "SELECT 1"; do
   >&2 echo "Database is unavailable - sleeping"
   sleep 1
@@ -9,9 +12,9 @@ done
 
 >&2 echo "Database is up - executing command"
 
-composer install
 
 bin/console doctrine:database:create --if-not-exists
 bin/console doctrine:migrations:migrate --no-interaction
+
 
 php-fpm
